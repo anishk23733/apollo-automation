ba3207f69c206c1647c965a953beade4f3813042
Anish Kachinthaya <anishk23733@gmail.com>
2023-06-22 14:34:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Add people from a company URL.')
-    parser.add_argument('company_url', help='The company URL to fetch people from.')
+    parser.add_argument('company_url', nargs='+', default=[], help='The company URLs to fetch people from.')
 
     args = parser.parse_args()
 
     api_key = os.environ.get('API_KEY')
+    company_urls = args.company_url
+
     apollo_client = Apollo(api_key)
-    company_url = args.company_url
+    
+    for url in company_urls:
+        successfully_added = apollo_client.get_and_add_people(url)
 
-    successfully_added = apollo_client.get_and_add_people(company_url)
-
-    print(f'[LOG] Successfully added {successfully_added} contacts.')
+        print(f'[LOG] Successfully added {successfully_added} contacts for {url}.')
