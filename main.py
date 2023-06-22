cfdecb9e0541b1ead3aba3ad853d3ddaf812b917
Anish Kachinthaya <anishk23733@gmail.com>
2023-06-22 14:26:19 -0400

    use environment variable and command line argument

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from apollo import Apollo
-
-API_KEY = ''
+import os
+import argparse
 
 if __name__ == '__main__':
-    apollo_client = Apollo(API_KEY)
-    company_url = 'tome.app'
+    parser = argparse.ArgumentParser(description='Add people from a company URL.')
+    parser.add_argument('company_url', help='The company URL to fetch people from.')
+
+    args = parser.parse_args()
+
+    api_key = os.environ.get('API_KEY')
+    apollo_client = Apollo(api_key)
+    company_url = args.company_url
 
     successfully_added = apollo_client.get_and_add_people(company_url)
 
